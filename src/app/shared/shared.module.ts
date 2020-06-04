a8d968d17ff65c6c63027eac6893b97dfd152958
Florian MARTIN <florian.martin63000@gmail.com>
2020-06-04 19:09:23 +0200

    Fix: delete an inexistant import in SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CustomValidatorsPipe } from './pipes/custom-validators.pipe';
-
 
 
 @NgModule({
-  declarations: [CustomValidatorsPipe],
+  declarations: [
+  ],
   imports: [
     CommonModule
   ]
